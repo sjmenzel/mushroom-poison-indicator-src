1666abd725105080b6393ae228c392675e1dfb8f
sjmenzel <sjmenzel@gmail.com>
2020-02-19 20:24:01 -0500

    Moved correlation calculation to its own function.

--- mushroom_indicator_mentor.py
+++ mushroom_indicator_mentor.py
@@ -14,6 +14,19 @@ TRAINED_FILE_NAME = 'mushroom_indicator_trained.py'
 TARGET_VARIABLE = 'mushroom'        # mushroom == 1, means mushroom is poisonous
 
 
+def find_best_attr(df, tv):
+    best_attr, best_coeff = None, 0
+
+    for attr, row in df.iterrows():
+        coeff = round(row[tv], 3)   # Rounded to 3 decimals
+
+        # Ideally -1.0 or +1.0 coefficient
+        if abs(coeff) > best_coeff and attr != tv:
+            best_attr, best_coeff = attr, coeff
+
+    return best_attr, best_coeff
+
+
 def main():
     # Read csv file
     df = pd.read_csv(TRAINING_DATASET)
@@ -24,14 +37,7 @@ def main():
     tv = TARGET_VARIABLE
 
     # Find the attribute that has the optimal coefficients in relation to the target variable
-    best_attr, best_coeff = None, 0
-
-    for attr, row in df.iterrows():
-        coeff = round(row[tv], 3)   # Rounded to 3 decimals
-
-        # Ideally -1.0 or +1.0 coefficient
-        if abs(coeff) > best_coeff and attr != tv:
-            best_attr, best_coeff = attr, coeff
+    best_attr, best_coeff = find_best_attr(df, tv)
 
     # Print results
     print('The attribute with the best coefficient is {} with value of {}.'.format(best_attr, best_coeff))
