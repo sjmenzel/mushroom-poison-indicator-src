52bd40faf2d43d2d90eab6fa028ea6bf2ff5cfd6
sjmenzel <sjmenzel@gmail.com>
2020-02-19 20:03:14 -0500

    Swapped out the dataset for a one-hot version (more appropriate for this project).

--- mushroom_indicator_mentor.py
+++ mushroom_indicator_mentor.py
@@ -4,3 +4,20 @@
     Author:
     - Samuel Menzel @ www.github.com/sjmenzel
 """
+
+import pandas as pd
+
+TRAINING_DATASET = 'mushroom_training_dataset.csv'
+
+TRAINED_FILE_NAME = 'mushroom_indicator_trained.py'
+
+TARGET_VARIABLE = 'mushroom'        # mushroom == 1, means mushroom is poisonous
+
+
+def main():
+    # Read csv file
+    df = pd.read_csv()
+
+
+if __name__ == '__main__':
+    main()
